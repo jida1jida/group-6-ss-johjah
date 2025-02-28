d919c42f819ead1ccee8e6ed15cb2f32cd1b2288
[Jake Ida] <[jida1@asu.edu]>
2025-02-27 21:08:05 -0700

    added HTML file for meditation page, and CSS and JS files for both meditation and main menu

---
+++ public/js/mainmenu.js
@@ -0,0 +1,49 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    //////////////////////////////////////////
+    //ELEMENTS TO ATTACH EVENT LISTENERS
+    //////////////////////////////////////////
+    const logoutButton = document.getElementById('logoutButton');
+    const refreshButton = document.getElementById('refreshButton');
+    //////////////////////////////////////////
+    //END ELEMENTS TO ATTACH EVENT LISTENERS
+    //////////////////////////////////////////
+
+
+    //////////////////////////////////////////
+    //EVENT LISTENERS
+    //////////////////////////////////////////
+    // Log out and redirect to login
+    logoutButton.addEventListener('click', () => {
+        localStorage.removeItem('token');
+        window.location.href = '/';
+    });
+
+    // Refresh list when the button is clicked
+    refreshButton.addEventListener('click', async () => {
+        renderUserList();
+    });
+    //////////////////////////////////////////
+    //END EVENT LISTENERS
+    //////////////////////////////////////////
+
+
+    //////////////////////////////////////////////////////
+    //CODE THAT NEEDS TO RUN IMMEDIATELY AFTER PAGE LOADS
+    //////////////////////////////////////////////////////
+    // Initial check for the token
+    const token = localStorage.getItem('jwtToken');
+    if (!token) {
+        window.location.href = '/';
+    } else {
+        DataModel.setToken(token);
+        renderUserList();
+    }
+    //////////////////////////////////////////
+    //END CODE THAT NEEDS TO RUN IMMEDIATELY AFTER PAGE LOADS
+    //////////////////////////////////////////
+});
+//END OF DOMCONTENTLOADED
+
+
